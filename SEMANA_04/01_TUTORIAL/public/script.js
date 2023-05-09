e4946a0bca5bc69d5b0c15a6f3cb36d9c550f7d6
Raissa de Cássia Moraes <raissa.paula@sou.inteli.edu.br>
2023-05-09 13:42:01 -0300

    Tutorial semana 4

--- SEMANA_04/01_TUTORIAL/public/script.js
+++ SEMANA_04/01_TUTORIAL/public/script.js
@@ -1,11 +1,10 @@
-﻿/* Função que calcula uma soma e mostra a operação realizada e o resultado obtido */
+/* Função que calcula uma soma e mostra a operação realizada e o resultado obtido */
 function calcula(x, y){
-    var op = "+";
-    var resultado;
-    if (op == "+") {
-        resultado = x + y;
-    };
-    document.getElementById("saida").innerHTML = `${x} ${op} ${y} = ${resultado}`;
-    console.log(`<br /> ${x} ${op} ${y} = ${resultado}`)
+	var op = "+";
+	var resultado;
+	if (op == "+") {
+		resultado = x + y;
+	};
+	document.getElementById("saida").innerHTML = `${x} ${op} ${y} = ${resultado}`;
+	console.log(`<br /> ${x} ${op} ${y} = ${resultado}`)
 }
-
